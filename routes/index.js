8a8f29c65d57399e3a5e480ee3b1c19dc6a13ac9
Nishanth Babu <jnishanthbabu@gmail.com>
2021-08-01 18:30:23 +0530

    react

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function (req, res, next) {
+  console.log("yes")
+  // res.render('index', { title: 'Express' });
+});
 
-router.get('/api/test', (req, res) => {
+router.get('/test', (req, res) => {
   let x = "Your response has been noted :) "
   res.status(200).send(x)
 })
